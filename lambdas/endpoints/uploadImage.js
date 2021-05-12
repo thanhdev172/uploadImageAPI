52d08aded1737d92775cba6e29a60f9d0b00d611
Nguyen Cong Thanh <muahangonlineabc@gmail.com>
2021-05-12 16:24:42 +0700

    done

--- lambdas/endpoints/uploadImage.js
+++ lambdas/endpoints/uploadImage.js
@@ -1,60 +1,64 @@
-import Responses from '../common/API_Responses';
-import * as fileType from 'file-type';
-import { v4 as uuid } from 'uuid';
-import * as AWS from 'aws-sdk';
+import Responses from "../common/API_Responses";
+import * as fileType from "file-type";
+import { v4 as uuid } from "uuid";
+import * as AWS from "aws-sdk";
+import { json } from "body-parser";
 
 const s3 = new AWS.S3();
 
-const allowedMimes = ['image/jpeg', 'image/png', 'image/jpg'];
+const allowedMimes = ["image/jpeg", "image/png", "image/jpg"];
 
-exports.handler = async event => {
-    try {
-        const body = JSON.parse(event.body);
+exports.handler = async (event) => {
+  try {
+    const body = JSON.parse(event.body);
 
-        if (!body || !body.image || !body.mime) {
-            return Responses._400({ message: 'incorrect body on request' });
-        }
-
-        if (!allowedMimes.includes(body.mime)) {
-            return Responses._400({ message: 'mime is not allowed ' });
-        }
-
-        let imageData = body.image;
-        if (body.image.substr(0, 7) === 'base64,') {
-            imageData = body.image.substr(7, body.image.length);
-        }
-
-        const buffer = Buffer.from(imageData, 'base64');
-        const fileInfo = await fileType.fromBuffer(buffer);
-        const detectedExt = fileInfo.ext;
-        const detectedMime = fileInfo.mime;
-
-        if (detectedMime !== body.mime) {
-            return Responses._400({ message: 'mime types dont match' });
-        }
-
-        const name = uuid();
-        const key = `${name}.${detectedExt}`;
-
-        console.log(`writing image to bucket called ${key}`);
-
-        await s3
-            .putObject({
-                Body: buffer,
-                Key: key,
-                ContentType: body.mime,
-                Bucket: process.env.imageUploadBucket,
-                ACL: 'public-read',
-            })
-            .promise();
-
-        const url = `https://${process.env.imageUploadBucket}.s3-${process.env.region}.amazonaws.com/${key}`;
-        return Responses._200({
-            imageURL: url,
-        });
-    } catch (error) {
-        console.log('error', error);
-
-        return Responses._400({ message: error.message || 'failed to upload image' });
+    if (!body || !body.image || !body.mime) {
+      return Responses._400({ message: "incorrect body on request" });
     }
+
+    if (!allowedMimes.includes(body.mime)) {
+      return Responses._400({ message: "mime is not allowed " });
+    }
+
+    let imageData = body.image;
+    if (body.image.substr(0, 7) === "base64,") {
+      imageData = body.image.substr(7, body.image.length);
+    }
+
+    const buffer = Buffer.from(imageData, "base64");
+    const fileInfo = await fileType.fromBuffer(buffer);
+    const detectedExt = fileInfo.ext;
+    const detectedMime = fileInfo.mime;
+
+    if (detectedMime !== body.mime) {
+      return Responses._400({ message: "mime types dont match" });
+    }
+
+    const originName = body.name.split(".")[0];
+    const name = originName + "-" + uuid();
+    const key = `${name}.${detectedExt}`;
+
+    console.log(`writing image to bucket called ${key}`);
+
+    await s3
+      .putObject({
+        Body: buffer,
+        Key: key,
+        ContentType: body.mime,
+        Bucket: process.env.imageUploadBucket,
+        ACL: "public-read",
+      })
+      .promise();
+
+    const url = `https://${process.env.imageUploadBucket}.s3-${process.env.region}.amazonaws.com/${key}`;
+
+    return Responses._200({
+      imageURL: url,
+    });
+  } catch (error) {
+    console.log("error", error);
+    return Responses._400({
+      message: error.message || "failed to upload image",
+    });
+  }
 };
